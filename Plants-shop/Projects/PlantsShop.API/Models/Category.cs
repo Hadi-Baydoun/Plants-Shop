8237ab52fe66d89b91769188d889408d8cd3b8f5
Hadi Baydoun <hadibaydoun17@gmail.com>
2024-06-16 22:02:42 +0300

    conflict fix

--- Plants-shop/Projects/PlantsShop.API/Models/Category.cs
+++ Plants-shop/Projects/PlantsShop.API/Models/Category.cs
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Net;
-
 namespace PlantsShop.API.Models;
-=======
-﻿namespace PlantsShop.API.Models;
->>>>>>> eb2e309353c8a0a0bfd5c1a2d2d16c464fccc270
 
 public partial class Category
 {
